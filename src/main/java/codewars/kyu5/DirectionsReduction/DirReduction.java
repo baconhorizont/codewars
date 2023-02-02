aa521c70dabed6b9e027ded1ea0b1c63cfa3191b
baconhorizont <bboard16@gmail.com>
2023-02-02 10:35:13 +0100

    kyu5 solution

--- src/main/java/codewars/kyu5/DirectionsReduction/DirReduction.java
+++ src/main/java/codewars/kyu5/DirectionsReduction/DirReduction.java
@@ -1,18 +1,72 @@
 package codewars.kyu5.DirectionsReduction;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DirReduction {
 
     public static String[] dirReduc(String[] arr) {
-        Map<String,Integer> counter = new HashMap<>(Map.of("NORTH", 0, "EAST", 0, "WEST", 0, "SOUTH", 0));
-
+        int shout = 0;
+        int north = 0;
+        int west = 0;
+        int east = 0;
+        List<String> result = new ArrayList<>();
         for (String actual: arr) {
-            counter.put(actual,counter.get(actual) +1);
+            if(actual.equals("NORTH")){
+                north++;
+            }
+            if(actual.equals("EAST")){
+                east++;
+            }
+            if(actual.equals("WEST")){
+                west++;
+            }
+            if(actual.equals("SOUTH")){
+                shout++;
+            }
         }
 
-        return new String[] {};
+        if(north == shout && west == east){
+            return new String[] {};
+        }
+
+        if(north > shout){
+            addToResult(result,"NORTH",north - shout);
+        } else if (shout > north){
+            addToResult(result,"SOUTH",shout - north);
+        }
+        if(west > east){
+            addToResult(result,"WEST",west - east);
+        } else if (east > west){
+            addToResult(result,"EAST",east - west);
+        }
+        return result.toArray(new String[0]);
+    }
+    private static void addToResult(List<String> result,String direction,int times){
+        for (int i = 0; i < times; i++) {
+            result.add(direction);
+        }
     }
 
 }
+
+
+
+//        Map<String,Integer> counter = new HashMap<>(Map.of("NORTH", 0, "EAST", 0, "WEST", 0, "SOUTH", 0));
+//        for (String actual: arr) {
+//            counter.put(actual,counter.get(actual) +1);
+//        }
+//        if(counter.get("NORTH") == counter.get("SOUTH") && counter.get("WEST") == counter.get("EAST")){
+//            return new String[] {};
+//        }
+//
+//        if(counter.get("NORTH") > counter.get("SOUTH")){
+//            addToResult(result,"NORTH",counter.get("SOUTH") - counter.get("NORTH"));
+//        } else if (counter.get("SOUTH") > counter.get("NORTH")){
+//            result.add("SOUTH");
+//        }
+//
+//        if(counter.get("WEST") > counter.get("EAST")){
+//            result.add("WEST");
+//        } else if (counter.get("EAST") > counter.get("WEST")){
+//            result.add("EAST");
+//        }
--- src/test/java/codewars/kyu5/DirectionsReduction/DirReductionTest.java
+++ src/test/java/codewars/kyu5/DirectionsReduction/DirReductionTest.java
@@ -8,8 +8,13 @@ class DirReductionTest {
     @Test
     public void testSimpleDirReduc() {
 
-        //assertArrayEquals(new String[]{"WEST"}, DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+        assertArrayEquals(new String[]{"WEST"}, DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
 
         assertArrayEquals(new String[]{}, DirReduction.dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"}));
+
+        assertArrayEquals(new String[]{"WEST","WEST"}, DirReduction.dirReduc(new String[]{"NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"}));
     }
 }
+
+
+
